5945f19b2e8d2460636f9a48d9cafd311727bdf4
ShengXue97 <simshengxue1997@gmail.com>
2021-01-11 21:52:33 +0800

    beta version 1

--- get_started_complete/code.js
+++ get_started_complete/code.js
@@ -1,16 +1,48 @@
+
+
 var thead = document.querySelector("table").childNodes[0];
 var theadRows = (thead.querySelectorAll("tr"));
 var theadCell = theadRows[0].appendChild(document.createElement('th'));
-theadCell.innerHTML = "SGT";
+theadCell.innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 var tbody = document.querySelector("table").childNodes[1];
 var rows = (tbody.querySelectorAll("tr"));
 rows.forEach(row => {
     var cells = row.querySelectorAll("td");
-    var day = cells[0].innerText;
-    var PTtime = cells[1].innerText;
+    var [day, timezone1] = cells[0].innerText.split(" ");
+    var [ETtime, am_pm, timezone2] = cells[2].innerText.split(" ");
 
     var td = row.appendChild(document.createElement('td'));
-    td.innerHTML = `${day};${PTtime}`;
-}
+
+    var date = new Date('January 11, 2021 ' + ETtime + " " + am_pm);
+
+    //5 because Eastern Tims is UTC - 5.
+    var result = calcTime(date, 5, day);
+    td.innerHTML = result;
+    }
 )
+
+
+function convertDay(currentDay, dayDifference){
+    const dayMappings = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    
+    console.log(`${currentDay} ${dayDifference}`)
+    var newDay = dayMappings[(dayMappings.indexOf(currentDay) + dayDifference) % 7];
+    return newDay;
+}
+
+function calcTime(date, offset, dayToCompare) {
+    
+    // add local time zone offset
+    // get UTC time in msec
+    const utc = date.getTime() + (3600000 * offset);
+    // create new Date object for different city
+    // using supplied offset
+    const nd = new Date(utc - (60000 * date.getTimezoneOffset()));
+    // return time as a string
+    const utcDate = new Date(utc);
+    console.log(`${nd} ${utcDate}`);
+    const changedDay = convertDay(dayToCompare, nd.getDay() - date.getDay())
+    const time = nd.toLocaleString().split(",")[1];
+    return `${changedDay},${time}`;
+}
