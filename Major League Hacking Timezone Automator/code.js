428b1b045d865e2980666464e95fd07bbdf42b29
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-11 23:31:16 +0800

    Clean up code

--- Major League Hacking Timezone Automator/code.js
+++ Major League Hacking Timezone Automator/code.js
@@ -9,42 +9,38 @@ tables.forEach(table => {
     theadCell.innerHTML = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     var tbody = table.childNodes[1];
-    var rows = (tbody.querySelectorAll("tr"));
+    var rows = tbody.querySelectorAll("tr");
     rows.forEach(row => {
         var cells = row.querySelectorAll("td");
         var [day, timezone1] = cells[0].innerText.split(" ");
         var [ETtime, am_pm, timezone2] = cells[2].innerText.split(" ");
 
         var td = row.appendChild(document.createElement('td'));
+        var date = new Date(`January 11, 2021 ${ETtime} ${am_pm}`);
 
-        var date = new Date('January 11, 2021 ' + ETtime + " " + am_pm);
-
-        //5 because Eastern Tims is UTC - 5.
+        //5 because Eastern Time is UTC - 5.
         var result = calcTime(date, 5, day);
         td.innerHTML = result;
-        }
+    }
     )
 }
 );
 
 
-function convertDay(currentDay, dayDifference){
+function convertDay(currentDay, dayDifference) {
     const dayMappings = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    
+
     var newDay = dayMappings[(dayMappings.indexOf(currentDay) + dayDifference) % 7];
     return newDay;
 }
 
 function calcTime(date, offset, dayToCompare) {
-    
-    // add local time zone offset
-    // get UTC time in msec
+    // get UTC time in milliseconds
     const utc = date.getTime() + (3600000 * offset);
     // create new Date object for different city
     // using supplied offset
     const nd = new Date(utc - (60000 * date.getTimezoneOffset()));
-    // return time as a string
-    const utcDate = new Date(utc);
+    // check if day is changed after time conversion
     const changedDay = convertDay(dayToCompare, nd.getDay() - date.getDay())
     const time = nd.toLocaleString().split(",")[1];
     return `${changedDay},${time}`;
