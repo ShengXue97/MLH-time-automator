f4c0e49ef2dc95a7f7fa7a252d16833ceb4f26bb
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-11 16:32:59 +0800

    Briefly edit page

--- get_started_complete/code.js
+++ get_started_complete/code.js
@@ -1,16 +1,16 @@
-var thead = document.querySelector("table").childNodes[0]; 
+var thead = document.querySelector("table").childNodes[0];
 var theadRows = (thead.querySelectorAll("tr"));
 var theadCell = theadRows[0].appendChild(document.createElement('th'));
 theadCell.innerHTML = "SGT";
 
-var tbody = document.querySelector("table").childNodes[1]; 
+var tbody = document.querySelector("table").childNodes[1];
 var rows = (tbody.querySelectorAll("tr"));
 rows.forEach(row => {
-        var cells = (row.querySelectorAll("td"));
-        var day = cells[0].innerText;
-        var PTtime = cells[1].innerText;
+    var cells = row.querySelectorAll("td");
+    var day = cells[0].innerText;
+    var PTtime = cells[1].innerText;
 
-        var td = row.appendChild(document.createElement('td'));
-        td.innerHTML = day + ";" + PTtime;
-    }
+    var td = row.appendChild(document.createElement('td'));
+    td.innerHTML = `${day};${PTtime}`;
+}
 )
